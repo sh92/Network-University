a8387fd31f919fb681f58ab619eb2283fa979936
Lee SangHee <sanghee.lee1992@gmail.com>
2016-10-16 06:28:27 +0900

    add chatProgram

---
+++ chatProgram/ThreadServer.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.connections = set()
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def serverMessage(self, client, address):
+        while True:
+            message = input('[Server]')
+            self.broadcast(message,client,address)
+            if message[0:4].lower() == 'quit':
+                break
+
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(600)
+            self.connections.add((client,address))
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def broadcast(self,data,client,address):
+        print("boradCast")
+        for conn,addr in self.connections:
+            if conn== client and addr == address:
+                continue
+            print(addr)
+            conn.send((data).encode())
+
+    def listenToClient(self, client, address):
+        size = 4096
+        threading.Thread(target = self.serverMessage,args = (client,address)).start()
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    response = data.decode('utf-8')
+                    print(response)
+                    print('[Server]')
+                    self.broadcast(response,client,address)
+                else:
+                    self.connections.remove((client,address)) 
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+
+if __name__ == "__main__":
+    port_num = input("Port? ")
+    ThreadedServer('',int(port_num)).listen()
---
+++ chatProgram/client.py
@@ -0,0 +1,46 @@
+import sys
+import socket
+import threading
+
+def recv_message(message_socket,client_ID):
+	while True:
+		message = message_socket.recv(4096).decode()
+		sys.stdout.write('\n'+message+'\n['+client_ID+']>>')
+		if message[7:11].lower() == 'quit':
+			break
+	print('Host Disconnected')
+
+def send_message(my_socket,client_ID):
+	while True:
+		sys.stdout.write('['+client_ID+']>>')
+		message = input('')
+		my_socket.send(('['+client_ID+']>>' + message).encode())
+		if message[0:4].lower() == 'quit':
+			break
+	print('Host Disconnect')
+	message = '\nExit chatroom'
+	my_socket.send((message+'['+client_ID+']' ).encode())
+	my_socket.close()
+	sys.exit()
+
+def main():
+	if len(sys.argv) !=4:
+		print("python client.py [IPADDRESS] [PORTNUMBER] [CLIENT ID]")
+		sys.exit()
+
+	ip_address = sys.argv[1]
+	port_number = int(sys.argv[2])
+	client_ID=  sys.argv[3]
+
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client_socket.connect((ip_address, port_number))
+
+	print("Host connected")
+	message = 'Enter chatroom'
+	client_socket.send((message+'['+client_ID+']' ).encode())
+	threading.Thread(target = recv_message, args=(client_socket,client_ID)).start()
+	threading.Thread(target = send_message, args=(client_socket,client_ID)).start()
+
+
+if __name__ == '__main__':
+	main()
