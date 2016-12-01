d3cdd911a4374cbced0fca8a4dd69ad293345a30
sh92 <sanghee.lee1992@gmail.com>
2016-12-01 09:24:59 +0900

    Start IRC project

---
+++ irc/irc.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+
+server = socket.gethostbyname('irc.freenode.org')
+channel = "#sanghee"
+botnick = "myNickName2"
+
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+print "connecting to:"+server
+irc.connect((server, 6667))                                                         #connects to the server
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun bot!\n") #user authentication
+irc.send("NICK "+ botnick +"\n")                            #sets nick
+irc.send("PRIVMSG nickserv :iNOOPE\r\n")    #auth
+irc.send("JOIN "+ channel +"\n")   
+while 1:    #puts it in a loop
+    text=irc.recv(2040)  #receive the text
+    print text 
+    if text.find('PING') != -1:                          #check if 'PING' is found
+        irc.send('PONG ' + text.split() [1] + '\r\n') #returnes 'PONG' back to the server (prevents pinging out!)
+
